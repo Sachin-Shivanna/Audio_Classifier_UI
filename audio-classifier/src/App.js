5788dd899c2bebc6f9ca2590a8bb1af4834422bc
Sachin Shivanna <sachin.shivanna01@gmail.com>
2024-03-13 10:54:21 -0500

    title changed

--- audio-classifier/src/App.js
+++ audio-classifier/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Audio Classifier UI
         </a>
       </header>
     </div>
